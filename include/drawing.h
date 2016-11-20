bf31d984a5d11c22460973167a9692aba584aa83
kash <krswshnt@gmail.com>
2016-11-21 01:51:07 +0900

    Add definition for drawing.

---
+++ include/drawing.h
@@ -0,0 +1,6 @@
+#ifndef __DRAWING_H__
+#define __DRAWING_H__
+
+void DrawClockHand();
+
+#endif /* __DRAWING_H__ */
---
+++ src/drawing.c
@@ -0,0 +1,6 @@
+#include "../include/drawing.h"
+
+void DrawClockHand()
+{
+    
+}
