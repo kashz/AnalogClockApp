3da26ba7847fb6c5a9d802ed9bf153b726d5a304
Shunta KARASAWA <krswshnt@gmail.com>
2016-12-03 07:25:04 +0900

    :bug: fix DrawEllipse()

--- src/drawing.c
+++ src/drawing.c
@@ -1,17 +1,27 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
 #include "../include/type.h"
+#include "../include/colors.h"
 #include "../include/drawing.h"
 #include <math.h>
 
+// Number of divisions for approximated polygons
+#define POLYGON_ACCURACY 100
+
 void DrawEllipse(int x, int y, int width, int height) {
-    int i, n = 25;
+    int i, n = POLYGON_ACCURACY;
     double rad;
-    Vector2d point;
+    Vector2i point;
 
     glBegin(GL_POLYGON);
-    for ( i = 0; i < part; i++ ) {
+    for ( i = 0; i < n; i++ ) {
         rad = 2 * M_PI * ((double)i / n);
-        point.x = width  * cos(rad);
-        point.y = height * sin(rad);
+        point.x = x + width  * cos(rad);
+        point.y = y + height * sin(rad);
         glVertex2i(point.x, point.y);
     }
     glEnd();
