8f6a2ff9405feb647186b80deca1b465e78556df
Shunta KARASAWA <krswshnt@gmail.com>
2016-12-03 06:09:00 +0900

    :+1: add DrawEllipse()

--- include/drawing.h
+++ include/drawing.h
@@ -1,6 +1,8 @@
 #ifndef __DRAWING_H__
 #define __DRAWING_H__
 
+void DrawEllipse(int x, int y, int width, int height);
+
 void DrawClockHand();
 
 #endif /* __DRAWING_H__ */
--- src/drawing.c
+++ src/drawing.c
@@ -1,6 +1,23 @@
+#include "../include/type.h"
 #include "../include/drawing.h"
+#include <math.h>
+
+void DrawEllipse(int x, int y, int width, int height) {
+    int i, n = 25;
+    double rad;
+    Vector2d point;
+
+    glBegin(GL_POLYGON);
+    for ( i = 0; i < part; i++ ) {
+        rad = 2 * M_PI * ((double)i / n);
+        point.x = width  * cos(rad);
+        point.y = height * sin(rad);
+        glVertex2i(point.x, point.y);
+    }
+    glEnd();
+}
 
 void DrawClockHand()
 {
-    
+
 }
