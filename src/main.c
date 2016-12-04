475015460f02374047e5e632e9d140b49cbaba49
Shunta KARASAWA <krswshnt@gmail.com>
2016-12-04 11:01:41 +0900

    introduce DrawClockHand()

--- src/main.c
+++ src/main.c
@@ -8,14 +8,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include "../include/drawing.h"
 
 void Display (void);
 void Reshape (int, int);
 void Timer(int value);
-void Write(int sec, int min, int hour, int w, int h);
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitWindowSize(300, 300);
     glutCreateWindow("Clock");
@@ -44,7 +43,7 @@ void Display (void)
     width = glutGet(GLUT_WINDOW_WIDTH);
     height = glutGet(GLUT_WINDOW_HEIGHT);
 
-    Write(t_time->tm_sec, t_time->tm_min, t_time->tm_hour, width, height);
+    DrawClockHand(width, height, t_time);
 
     glFlush();
     // glutSwapBuffers();
@@ -64,44 +63,4 @@ void Timer(int value)
 {
     glutPostRedisplay();
     glutTimerFunc(500, Timer, 0);
-}
-void Write(int sec, int min, int hour, int w, int h)
-{
-    int x, y;
-    double theta;
-    int xc = w/2;
-    int yc = h/2;
-    int l = 100;
-
-    // Seconds
-    theta = (2*M_PI/60) * (double)sec;
-    x = (double)xc + (double)l*sin(theta);
-    y = (double)yc - (double)l*cos(theta);
-    glBegin(GL_LINES);
-    glColor3ub(0, 0, 0);
-    glVertex2i(xc, yc);
-    glVertex2i(x, y);
-    glEnd();
-
-    // Minutes
-    theta = (2*M_PI/3600) * (double)(60*min+sec);
-    l = 100;
-    x = (double)xc + (double)l*sin(theta);
-    y = (double)yc - (double)l*cos(theta);
-    glBegin(GL_LINES);
-    glColor3ub(1, 0, 0);
-    glVertex2i(xc, yc);
-    glVertex2i(x, y);
-    glEnd();
-
-    // Hour
-    theta = (2*M_PI/43200) * (double)(3600*hour + 60*min+sec);
-    l = 80;
-    x = (double)xc + (double)l*sin(theta);
-    y = (double)yc - (double)l*cos(theta);
-    glBegin(GL_LINES);
-    glColor3ub(0, 1, 0);
-    glVertex2i(xc, yc);
-    glVertex2i(x, y);
-    glEnd();
 }
