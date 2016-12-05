b35b9cd773c827dc24c5e22518d73498512fb87f
Shunta KARASAWA <krswshnt@gmail.com>
2016-12-06 04:48:14 +0900

    add store

--- include/store.h
+++ include/store.h
@@ -6,5 +6,18 @@
 #ifndef __STORE_H__
 #define __STORE_H__
 
+#include "./color.h"
+
+struct _store {
+    ColorRGB256 clockDialColor;
+    ColorRGB256 clockHandColor;
+    ColorRGB256 clockSecColor;
+};
+typedef struct _store Store;
+
+
+extern Store store;
+
+void InitStore();
 
 #endif /* __STORE_H__ */
--- src/store.c
+++ src/store.c
@@ -4,3 +4,13 @@
  */
 
 #include "../include/store.h"
+
+
+// Global store
+Store store;
+
+void InitStore() {
+    store.clockDialColor = SetColorRGB256(62, 63, 79);
+    store.clockHandColor = store.clockDialColor;
+    store.clockSecColor  = SetColorRGB256(239, 96, 96);
+}
