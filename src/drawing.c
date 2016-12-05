910be4a1bc5cee80e5a166c16eadc52ddc1bd1af
Shunta KARASAWA <krswshnt@gmail.com>
2016-12-06 04:48:44 +0900

    replace store

--- src/drawing.c
+++ src/drawing.c
@@ -11,6 +11,7 @@
 
 #include "../include/type.h"
 #include "../include/state.h"
+#include "../include/store.h"
 #include "../include/color.h"
 #include "../include/drawing.h"
 #include <math.h>
@@ -58,34 +59,25 @@ void DrawEllipseWithLine(int x, int y, int width, int height, float weight) {
 
 void DrawClock(int width, int height, struct tm *t_time) {
     Vector2i point;
-    ColorRGB256 lineColor, secHandColor;
     int size, len;
     double theta, clockFaceIncrement;
-
-    // Set Color
-    lineColor.red = 62;
-    lineColor.green = 63;
-    lineColor.blue = 79;
-    secHandColor.red = 239;
-    secHandColor.green = 96;
-    secHandColor.blue = 96;
 
     // Decide Size
     // Align to the smaller value.
     size = (width < height) ? (width/2*0.9) : (height/2*0.9);
 
     // Draw edge of clock
-    SetGLColorRGB256(lineColor);
+    SetGLColorRGB256(store.clockDialColor);
     DrawEllipseWithLine(width/2, height/2, size, size, 5);
     DrawEllipse(width/2, height/2, size * 0.08, size * 0.08);
 
     // Draw face of clock
     glLineWidth(6);
     glBegin(GL_LINES);
-    if (state.clockFaceType == CLOCK_FACE_NORMAL) {
+    if (state.clockFaceType == CLOCK_DIAL_NORMAL) {
         clockFaceIncrement = (M_PI)/2;
     }
-    else if (state.clockFaceType == CLOCK_FACE_ALL) {
+    else if (state.clockFaceType == CLOCK_DIAL_ALL) {
         clockFaceIncrement = (M_PI)/6;
     }
     for ( theta = 0; theta < (2 * M_PI);theta += clockFaceIncrement ) {
@@ -100,6 +92,8 @@ void DrawClock(int width, int height, struct tm *t_time) {
     }
     glEnd();
 
+
+    SetGLColorRGB256(store.clockHandColor);
     // Minutes
     theta = (2 * M_PI / 3600) * (double)(60 * t_time->tm_min + t_time->tm_sec);
     len = size * 0.9;
@@ -127,7 +121,7 @@ void DrawClock(int width, int height, struct tm *t_time) {
     theta = (2*M_PI/60) * (double)(t_time->tm_sec);
     point.x = width/2  + (double)len * sin(theta);
     point.y = height/2 - (double)len * cos(theta);
-    SetGLColorRGB256(secHandColor);
+    SetGLColorRGB256(store.clockSecColor);
     DrawEllipse(width/2, height/2, size * 0.05, size * 0.05);
     glLineWidth(3);
     glBegin(GL_LINES);
